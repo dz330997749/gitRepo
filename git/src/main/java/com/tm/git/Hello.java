cf767566ae638829e08a049a20cda6ccfbc95fa5
dz <dz@itcast.cn>
2018-09-10 11:01:24 +0800

    idea 提交

---
+++ git/src/main/java/com/tm/git/Hello.java
@@ -0,0 +1,19 @@
+package com.tm.git;
+
+/**
+ * @Author: duanzhu
+ * @Date: 2018/9/10 10:48
+ * @Description:
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+    }
+}
